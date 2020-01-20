41fe5fb549fac5751c04e7a991507b2e85c8143d
fbngrm <fbngrm@gmail.com>
2020-01-20 17:16:56 +0100

    use timestamp with tz

--- initdb/schema.sql
+++ initdb/schema.sql
@@ -6,17 +6,37 @@ CREATE TABLE time_records (
   id BIGSERIAL PRIMARY KEY,
   user_id INT REFERENCES users(id) NOT NULL,
   name varchar(256),
-  start_time TIMESTAMP NOT NULL,
-  start_time_tz varchar(50) NOT NULL,
-  stop_time TIMESTAMP NOT NULL,
-  stop_time_tz varchar(50) NOT NULL
+  start_time TIMESTAMP WITH TIME ZONE NOT NULL,
+  start_time_loc varchar(50) NOT NULL,
+  stop_time TIMESTAMP WITH TIME ZONE NOT NULL,
+  stop_time_loc varchar(50) NOT NULL
 );
 
-INSERT INTO users(id) VALUES
-(42);
+INSERT INTO users(id) VALUES(42);
 
-INSERT INTO time_records(user_id,name,start_time,start_time_tz,stop_time,stop_time_tz) VALUES
-(42,'foo','2020-01-01 00:00:00','Europe/Berlin','2020-01-01 02:00:00','Europe/Berlin'),
-(42,'bar','2020-01-19 12:00:00','Europe/Berlin','2020-01-19 16:00:00','Europe/Berlin');
-
-
+INSERT INTO
+  time_records(
+    user_id,
+    name,
+    start_time,
+    start_time_loc,
+    stop_time,
+    stop_time_loc
+  )
+VALUES
+  (
+    42,
+    'foo',
+    '2020-01-01 00:00:00+01', /* 1577833200 */
+    'Europe/Berlin',
+    '2020-01-01 01:00:00+01', /* 1577836800 */
+    'Europe/Berlin'
+  ),
+  (
+    42,
+    'foo',
+    '2020-01-20 00:00:00+01', /* 1579474800 */
+    'Europe/Berlin',
+    '2020-01-20 01:00:00+01', /* 1579478400 */
+    'Europe/Berlin'
+  )
