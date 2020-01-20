3585afe4ee2ca5922dee11032d1673448d2c9dcf
fbngrm <fbngrm@gmail.com>
2020-01-20 22:02:52 +0100

    remove headline

--- time-tracker-frontend/src/components/Picker.js
+++ time-tracker-frontend/src/components/Picker.js
@@ -6,7 +6,6 @@ export default class Picker extends Component {
     const { value, onChange, options } = this.props
     return (
       <span>
-        <h1>{value}</h1>
         <select onChange={e => onChange(e.target.value)} value={value}>
           {options.map(option => (
             <option value={option} key={option}>
