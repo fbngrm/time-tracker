f551dff67d735d53481263e188e107f6610ebc35
fbngrm <fbngrm@gmail.com>
2020-01-22 17:34:12 +0100

    render AddRecord and RecordList in Root component

--- time-tracker-frontend/src/containers/Root.js
+++ time-tracker-frontend/src/containers/Root.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import configureStore from '../configureStore'
 import AsyncApp from './AsyncApp'
+import AddRecord from './AddRecord'
+import VisibleRecordList from './VisibleRecordList'
 
 const store = configureStore()
 
@@ -10,7 +12,11 @@ export default class Root extends Component {
     return (
       <Provider store={store}>
         <AsyncApp />
+        <AddRecord />
+        <VisibleRecordList />
       </Provider>
     )
   }
 }
+
+
