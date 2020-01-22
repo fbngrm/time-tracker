63565504bc81e1f3ff66bba72861d039bb73b554
fbngrm <fbngrm@gmail.com>
2020-01-22 17:32:24 +0100

    split reducers to separate files

--- time-tracker-frontend/src/reducers/index.js
+++ time-tracker-frontend/src/reducers/index.js
@@ -1,66 +1,11 @@
 import { combineReducers } from 'redux'
-import {
-  SELECT_PERIOD,
-  INVALIDATE_PERIOD,
-  REQUEST_RECORDS,
-  RECEIVE_RECORDS
-} from './actions'
-
-function selectedPeriod(state = 'day', action) {
-  switch (action.type) {
-    case SELECT_PERIOD:
-      return action.period
-    default:
-      return state
-  }
-}
-
-function records(
-  state = {
-    isFetching: false,
-    didInvalidate: false,
-    items: []
-  },
-  action
-) {
-  switch (action.type) {
-    case INVALIDATE_PERIOD:
-      return Object.assign({}, state, {
-        didInvalidate: true
-      })
-    case REQUEST_RECORDS:
-      return Object.assign({}, state, {
-        isFetching: true,
-        didInvalidate: false
-      })
-    case RECEIVE_RECORDS:
-      return Object.assign({}, state, {
-        isFetching: false,
-        didInvalidate: false,
-        items: action.records,
-        lastUpdated: action.receivedAt
-      })
-    default:
-      return state
-  }
-}
-
-function recordsByPeriod(state = {}, action) {
-  switch (action.type) {
-    case INVALIDATE_PERIOD:
-    case RECEIVE_RECORDS:
-    case REQUEST_RECORDS:
-      return Object.assign({}, state, {
-        [action.period]: records(state[action.period], action)
-      })
-    default:
-      return state
-  }
-}
+import { recordsByPeriod, selectedPeriod } from './period'
+import records from './records'
 
 const rootReducer = combineReducers({
   recordsByPeriod,
-  selectedPeriod
+  selectedPeriod,
+  records
 })
 
 export default rootReducer
