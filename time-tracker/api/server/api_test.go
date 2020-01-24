b1533b18ea8e9f9c3691f0efb57e1ed0ff6afd6c
fbngrm <fbngrm@gmail.com>
2020-01-24 22:12:06 +0100

    test ServeHTTP/Create

--- time-tracker/api/server/api_test.go
+++ time-tracker/api/server/api_test.go
@@ -1,227 +1,134 @@
 package server
 
-// type mockRosterStore struct{}
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
 
-// // uses the roster id to get the test data.
-// func (rs *mockRosterStore) Get(ctx context.Context, rosterID uint64) (*store.Roster, error) {
-// 	return rosterTests[rosterID].r, rosterTests[rosterID].e
-// }
+	"github.com/PentoHQ/tech-challenge-time/time-tracker/store"
+)
 
-// // test cases indexed by roster id
-// var rosterTests = map[uint64]struct {
-// 	d string        // description of test case
-// 	r *store.Roster // mock store response
-// 	e error         // mock store error
-// 	p string        // url path for test requests
-// 	s int           // expected http status code
-// 	b []byte        // expected payload of response
-// }{
-// 	// url path errors
-// 	0: { // 404
-// 		d: "expect missing id to result in 404",
-// 		p: "roster/",
-// 		s: http.StatusNotFound,
-// 		b: []byte("404 page not found"),
-// 	},
-// 	1: { // 404
-// 		d: "expect invalid status to result in 404",
-// 		p: "roster/1/players/invalid",
-// 		s: http.StatusNotFound,
-// 		b: []byte("404 page not found"),
-// 	},
-// 	// store errors
-// 	2: { // 500
-// 		d: "expect store error to result in 500 when requesting roster",
-// 		p: "roster/2", // roster id is the test case id used by the mock store
-// 		e: errInternal,
-// 		s: http.StatusInternalServerError,
-// 		b: []byte(fmt.Sprintf(`{"error":"%s"}`, errInternal.Error())),
-// 	},
-// 	3: { // 500
-// 		d: "expect store error to result in 500 when requesting active players",
-// 		p: "roster/3/active",
-// 		e: errInternal,
-// 		s: http.StatusInternalServerError,
-// 		b: []byte(fmt.Sprintf(`{"error":"%s"}`, errInternal.Error())),
-// 	},
-// 	// success
-// 	4: { // 200
-// 		d: "expect success when requesting roster",
-// 		p: "roster/4",
-// 		r: testdata.Rosters[382574876546039808].R,
-// 		s: http.StatusOK,
-// 		b: []byte(testdata.Rosters[382574876546039808].RS),
-// 	},
-// 	5: { // 200
-// 		d: "expect success when requesting active players",
-// 		p: "roster/5/active",
-// 		r: testdata.Rosters[382574876546039808].R,
-// 		s: http.StatusOK,
-// 		b: []byte(testdata.Rosters[382574876546039808].AP),
-// 	},
-// 	6: { // 200
-// 		d: "expect success when requesting benched players",
-// 		p: "roster/6/benched",
-// 		r: testdata.Rosters[382574876546039808].R,
-// 		s: http.StatusOK,
-// 		b: []byte(testdata.Rosters[382574876546039808].BP),
-// 	},
-// }
+var (
+	startCreateTest time.Time
+	stopCreateTest  time.Time
+)
 
-// func TestGet(t *testing.T) {
-// 	// service initialized with a mock store to
-// 	// control the rosters and errors we return
-// 	rs := &rosterService{
-// 		&mockRosterStore{},
-// 		200 * time.Millisecond,
-// 	}
+func init() {
+}
 
-// 	router := mux.NewRouter()
-// 	router.Handle("/roster/{id:[0-9]+}", rs).Methods("GET")
-// 	router.Handle(fmt.Sprintf("/roster/{id:[0-9]+}/{status:(?:%s|%s)}", Active, Benched), rs).Methods("GET")
+// uses the user id to get the test data.
+type mockTimeRecordStore struct{}
 
-// 	s := httptest.NewServer(router)
-// 	defer s.Close()
-// 	c := s.Client()
+func (rs *mockTimeRecordStore) Create(ctx context.Context, r store.TimeRecord) (*store.TimeRecord, error) {
+	var tr store.TimeRecord
+	decoder := json.NewDecoder(strings.NewReader(createRecordTests[r.UserID].p))
+	decoder.DisallowUnknownFields() // catch unwanted fields
+	if err := decoder.Decode(&tr); err != nil {
+		return nil, err
+	}
+	tr.RecordID = r.UserID
+	return &tr, createRecordTests[r.UserID].e
+}
+func (rs *mockTimeRecordStore) Get(ctx context.Context, userID uint64, t time.Time) ([]store.TimeRecord, error) {
+	// return getRecordTests[userID].r, getRecordTests[userID].e
+	return nil, nil
+}
 
-// 	for _, tc := range rosterTests {
-// 		tt := tc
-// 		t.Run(tt.d, func(t *testing.T) {
-// 			req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/%s", s.URL, tt.p), nil)
-// 			if err != nil {
-// 				t.Fatalf("unexpected err: %v", err)
-// 			}
-// 			resp, err := c.Do(req)
-// 			if err != nil {
-// 				t.Fatalf("unexpected err: %v", err)
-// 			}
-// 			// expected result
-// 			if want, got := tt.s, resp.StatusCode; want != got {
-// 				t.Errorf("want status code %d got %d", want, got)
-// 			}
-// 			body, err := ioutil.ReadAll(resp.Body)
-// 			if err != nil {
-// 				t.Fatalf("unexpected err: %v", err)
-// 			}
-// 			resp.Body.Close()
-// 			if want, got := tt.b, body; string(want) == string(got) {
-// 				t.Errorf("want response\n%+s\ngot\n%+s", want, got)
-// 			}
-// 		})
-// 	}
-// }
+// test cases indexed by id
+var createRecordTests = map[uint64]struct {
+	d string // description of test case
+	e error  // mock store error
+	p string // request payload
+	u string // route of the test request
+	s int    // expected http status code
+	b []byte // expected payload
+}{
+	// errors
+	0: {
+		d: "expect missing path segment to result in 404",
+		u: "/",
+		s: http.StatusNotFound,
+		b: []byte("404 page not found"),
+	},
+	1: { // 500
+		d: "expect mal-formed JSON payload to result in 500",
+		e: errInternal,
+		u: "record",
+		p: `{"user_id":2`, // missing closing brace
+		s: http.StatusInternalServerError,
+		b: []byte(fmt.Sprintf(`{"error":"%s"}`, errInternal.Error())),
+	},
+	2: { // 500
+		d: "expect store error to result in 500",
+		e: errInternal,
+		u: "record",
+		p: `{"user_id":2}`, // user_id is the testcase-id used by the mock store
+		s: http.StatusInternalServerError,
+		b: []byte(fmt.Sprintf(`{"error":"%s"}`, errInternal.Error())),
+	},
+	// success
+	3: {
+		d: "expect to successfully create and return a time record",
+		u: "record",
+		p: `{"user_id":3,"name":"foo","start_time":1577833200,"start_loc":"Europe/Berlin","stop":1577836800,"stop_loc":"Europe/Berlin", "duration":3600}`,
+		s: http.StatusOK,
+		b: []byte(`{"record_id":3,"user_id":3,"name":"foo","start_time":"01 Jan 2020 00:00:00","start_loc":"Europe/Berlin","stop":"01 Jan 2020 01:00:00","stop_loc":"Europe/Berlin", "duration":"01:00:00"}`),
+	},
+}
 
-// // we use the player id to detemine the return values.
-// type mockPlayerStore struct{}
+func getDateFromString(t *testing.T, date string) time.Time {
+	ti, err := time.Parse(time.RFC3339, date)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return ti
+}
 
-// // uses the players id to get the test data.
-// func (ps *mockPlayerStore) Insert(ctx context.Context, player store.Player) (*store.Player, error) {
-// 	return insertTests[player.PlayerID].r, insertTests[player.PlayerID].e
-// }
+func TestServeHTTPCreate(t *testing.T) {
+	// service initialized with a mock store to
+	// control the data and errors we return
+	rs := &timeRecordService{
+		&mockTimeRecordStore{},
+		200 * time.Millisecond,
+	}
+	// test server
+	s := httptest.NewServer(rs)
+	defer s.Close()
+	c := s.Client()
 
-// // uses the players id to get the test data.
-// func (ps *mockPlayerStore) Update(ctx context.Context, player store.Player) (*store.Player, error) {
-// 	return updateTests[player.PlayerID].r, updateTests[player.PlayerID].e
-// }
-
-// // uses the active players id to get the test data.
-// func (ps *mockPlayerStore) ChangePlayers(ctx context.Context, players store.PlayerChange) (*store.PlayerChange, error) {
-// 	return changeTests[players.Active.PlayerID].r, changeTests[players.Active.PlayerID].e
-// }
-
-// // test cases indexed by player id
-// var insertTests = map[uint64]struct {
-// 	d string        // description of test case
-// 	r *store.Player // mock store response
-// 	e error         // mock store error
-// 	u string        // request url path
-// 	p string        // request payload
-// 	s int           // expected http status code
-// 	b []byte        // expected payload
-// }{
-// 	// url path errors
-// 	0: { // 404
-// 		d: "expect missing path segment to result in 404",
-// 		u: "players/",
-// 		s: http.StatusNotFound,
-// 		b: []byte("404 page not found"),
-// 	},
-// 	// missing body errors
-// 	1: { // 400
-// 		d: "expect missing body to result in 400 when adding player",
-// 		u: "players/add",
-// 		s: http.StatusBadRequest,
-// 		b: []byte(fmt.Sprintf(`{"error":"%s"}`, errBadRequest.Error())),
-// 	},
-// 	// store errors
-// 	2: { // 500
-// 		d: "expect store error to result in 500 when adding player",
-// 		e: errInternal,
-// 		u: "players/add",
-// 		p: `{"player_id":2}`, // id is the testcase-id used by the mock store
-// 		s: http.StatusInternalServerError,
-// 		b: []byte(fmt.Sprintf(`{"error":"%s"}`, errInternal.Error())),
-// 	},
-// 	// success
-// 	3: {
-// 		d: "expect 200 when adding player",
-// 		u: "players/add",
-// 		r: &store.Player{
-// 			PlayerID:  3,
-// 			RosterID:  0,
-// 			FirstName: "foo",
-// 			LastName:  "bar",
-// 			Alias:     "foobar",
-// 			Status:    "active",
-// 		},
-// 		p: `{"player_id":3,"roster_id":0,"first_name":"foo","last_name":"bar","alias":"foobar","status":"active"}`,
-// 		s: http.StatusOK,
-// 		b: []byte(`{"player_id":3,"roster_id":0,"first_name":"foo","last_name":"bar","alias":"foobar","status":"active"}`),
-// 	},
-// }
-
-// func TestInsert(t *testing.T) {
-// 	// service initialized with a mock store to
-// 	// control the data and errors we return
-// 	ps := &playerService{
-// 		&mockPlayerStore{},
-// 		200 * time.Millisecond,
-// 	}
-
-// 	router := mux.NewRouter()
-// 	router.Handle("/players/add", ps).
-// 		Methods("POST")
-
-// 	s := httptest.NewServer(router)
-// 	defer s.Close()
-// 	c := s.Client()
-
-// 	for _, tc := range insertTests {
-// 		tt := tc
-// 		t.Run(tt.d, func(t *testing.T) {
-// 			req, err := http.NewRequest("POST", fmt.Sprintf("%s/%s", s.URL, tt.u), strings.NewReader(tt.p))
-// 			if err != nil {
-// 				t.Fatalf("unexpected err: %v", err)
-// 			}
-// 			resp, err := c.Do(req)
-// 			if err != nil {
-// 				t.Fatalf("unexpected err: %v", err)
-// 			}
-// 			// expected result
-// 			if want, got := tt.s, resp.StatusCode; want != got {
-// 				t.Errorf("want status code %d got %d", want, got)
-// 			}
-// 			body, err := ioutil.ReadAll(resp.Body)
-// 			if err != nil {
-// 				t.Fatalf("unexpected err: %v", err)
-// 			}
-// 			resp.Body.Close()
-// 			if want, got := tt.b, body; bytes.Compare(want, got) == 1 {
-// 				t.Errorf("want response\n%+s\ngot\n%+s", want, got)
-// 			}
-// 		})
-// 	}
-// }
+	for _, tc := range createRecordTests {
+		tt := tc
+		t.Run(tt.d, func(t *testing.T) {
+			req, err := http.NewRequest("POST", fmt.Sprintf("%s/%s", s.URL, tt.u), strings.NewReader(tt.p))
+			if err != nil {
+				t.Fatalf("unexpected err: %v", err)
+			}
+			resp, err := c.Do(req)
+			if err != nil {
+				t.Fatalf("unexpected err: %v", err)
+			}
+			// expected result
+			if want, got := tt.s, resp.StatusCode; want != got {
+				t.Errorf("want status code %d got %d", want, got)
+			}
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("unexpected err: %v", err)
+			}
+			resp.Body.Close()
+			if want, got := tt.b, body; bytes.Compare(want, got) == 1 {
+				t.Errorf("want response\n%+s\ngot\n%+s", want, got)
+			}
+		})
+	}
+}
 
 // // test cases indexed by player id
 // var updateTests = map[uint64]struct {
