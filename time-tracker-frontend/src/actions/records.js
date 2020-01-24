71d6c3192422eb281786f30c1c3a4b28feb220ab
fbngrm <fbngrm@gmail.com>
2020-01-24 19:58:55 +0100

    handle errors when fetching records

--- time-tracker-frontend/src/actions/records.js
+++ time-tracker-frontend/src/actions/records.js
@@ -3,6 +3,7 @@ export const REQUEST_RECORDS = 'REQUEST_RECORDS'
 export const RECEIVE_RECORDS = 'RECEIVE_RECORDS'
 export const SELECT_PERIOD = 'SELECT_PERIOD'
 export const INVALIDATE_PERIOD = 'INVALIDATE_PERIOD'
+export const FETCH_RECORDS_FAIL = 'FETCH_RECORDS_FAIL'
 
 export function selectPeriod(period) {
   return {
@@ -43,9 +44,28 @@ function fetchRecords(period){
     const timestamp = Math.floor(Date.now() / 1000)
     const url = `http://localhost:8080/records?user_id=${userID}&ts=${timestamp}&tz=${timezone}&period=${period}`
     return fetch(url)
-      .then(response => response.json())
-      .then(json => dispatch(receiveRecords(period, json)))
-  }
+      // Try to parse the response
+      .then(response =>
+        response.json().then(json => ({
+          status: response.status,
+          json
+        })
+      ))
+      .then(
+        // Both fetching and parsing succeeded
+        ({ status, json }) => {
+          if (status >= 400) {
+            dispatch({type: FETCH_RECORDS_FAIL, err: "error: status code "+status})
+          } else {
+            dispatch(receiveRecords(period, json))
+          }
+        },
+        // Either fetching or parsing failed!
+        err => {
+          dispatch({type: FETCH_RECORDS_FAIL, err: err.message})
+        }
+      )
+    }
 }
 
 function shouldFetchRecords(state, period) {
