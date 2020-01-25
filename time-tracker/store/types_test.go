389f300d18f0cac6615556d137c769b0d4f852b9
fbngrm <fbngrm@gmail.com>
2020-01-25 15:39:31 +0100

    test Marshal of TimeRecord

--- time-tracker/store/types_test.go
+++ time-tracker/store/types_test.go
@@ -79,3 +79,63 @@ func TestUnmarshall(t *testing.T) {
 		}
 	}
 }
+
+type marshalTest struct {
+	d   string           // test case description
+	in  store.TimeRecord // input
+	out []byte           // expected output
+	e   error            // expected error
+}
+
+func TestMarshall(t *testing.T) {
+	initLoc(t, "Europe/London")
+	initLoc(t, "Asia/Tokyo")
+	marhsalTests := []marshalTest{
+		marshalTest{
+			out: []byte(`{"record_id":0,"user_id":3,"name":"foo","start_time":"31 Dec 2019 23:00:00","start_loc":"Europe/London","stop_time":"01 Jan 2020 00:00:00","stop_loc":"Europe/London","duration":"01:00:00"}`),
+			in: store.TimeRecord{
+				RecordID: 0,
+				UserID:   3,
+				Name:     "foo",
+				Start:    time.Date(2019, time.December, 31, 23, 0, 0, 0, locs["Europe/London"]),
+				StartLoc: "Europe/London",
+				Stop:     time.Date(2020, time.January, 01, 0, 0, 0, 0, locs["Europe/London"]),
+				StopLoc:  "Europe/London",
+				Duration: 3600,
+			},
+		},
+		marshalTest{
+			out: []byte(`{"record_id":0,"user_id":3,"name":"foo","start_time":"01 Jan 2020 08:00:00","start_loc":"Asia/Tokyo","stop_time":"01 Jan 2020 09:00:00","stop_loc":"Asia/Tokyo","duration":"01:00:00"}`),
+			in: store.TimeRecord{
+				RecordID: 0,
+				UserID:   3,
+				Name:     "foo",
+				Start:    time.Date(2020, time.January, 1, 8, 0, 0, 0, locs["Asia/Tokyo"]),
+				StartLoc: "Asia/Tokyo",
+				Stop:     time.Date(2020, time.January, 1, 9, 0, 0, 0, locs["Asia/Tokyo"]),
+				StopLoc:  "Asia/Tokyo",
+				Duration: 3600,
+			},
+		},
+	}
+	for _, tc := range marhsalTests {
+		gotB, gotErr := tc.in.MarshalJSON()
+		// unexpected errors
+		if gotErr != nil && tc.e == nil {
+			t.Fatalf("%s: unexpected err: %v", tc.d, gotErr)
+		}
+		// expected errors
+		if gotErr == nil && tc.e != nil {
+			t.Fatalf("%s: expected err: %v", tc.d, tc.e)
+		}
+		if gotErr != nil && tc.e != nil {
+			if got, want := gotErr.Error(), tc.e.Error(); got != want {
+				t.Errorf("%s:\nwant err\n%+v\ngot\n%+v", tc.d, want, got)
+			}
+			continue
+		}
+		if got, want := string(gotB), string(tc.out); got != want {
+			t.Errorf("%s:\nwant time\n%+v\ngot\n%+v", tc.d, want, got)
+		}
+	}
+}
